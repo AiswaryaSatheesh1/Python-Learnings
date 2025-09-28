2ea340b2d237ffa6f521ef2faeb03d3ed49aa310
Aiswarya Satheesh <aishuaiswarya2051@gmail.com>
2025-09-28 15:13:06 +0530

    simple calculator program

---
+++ 2.simplecalculator.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter a num: "))
+num2 = int(input("Enter a num: "))
+
+result = num1 / num2 if num2 != 0 else "Undefined"
+
+print("sum: ",num1 + num2)
+print("diff: ", num1 - num2)
+print("prod: ", num1 * num2)
+print(f"quo: {result} ")
+
